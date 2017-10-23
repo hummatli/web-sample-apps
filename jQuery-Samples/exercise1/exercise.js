c1c4d04aae37119990807f7ba0e87b32163a99c8
Sattar Hummatli <settarxan@gmail.com>
2017-10-23 06:50:15 +0400

    Made exercise about jQuery Selection

---
+++ jQuery-Samples/exercise1/exercise.js
@@ -0,0 +1,11 @@
+//if(jQuery){
+	$("div").css("backgroundColor", "purple")
+	$("div.highlight").css("width", "200px")
+	$("#third").css("border", "2px solid orange")
+	//$("div:nth-of-type(1)").css("color", "pink") // nthof type ile
+	$("div:first-of-type").css("color", "pink") //birinci
+	//$("div:first").css("color", "pink") //jQuerynin first shortcaut-i ile. Bu yavash ishleyir
+
+// } else {
+// 	alert("No jQuery loaded")	
+// }
